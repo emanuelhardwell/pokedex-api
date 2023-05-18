697352f6d1524c66d16c8ac053490d43d5a490d5
emanuelhardwell <emanuelnoe16@gmail.com>
2023-05-17 21:50:40 -0600

    feat: The logger morgan was configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(morgan('dev'));
+
   await app.listen(3000);
+  console.log(`Server running on -----> ${await app.getUrl()}`);
 }
 bootstrap();
