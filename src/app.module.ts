3140434984f30aa8c32734339d3a72e5f00030e5
emanuelhardwell <emanuelnoe16@gmail.com>
2023-05-19 22:28:41 -0600

    feat: Mongodb was configured on nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost/nest-pokemon'),
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
