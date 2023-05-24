40e89581c3d2abd25a902322e8d68bb933c4b6be
emanuelhardwell <emanuelnoe16@gmail.com>
2023-05-23 22:40:26 -0600

    feat: The executeSeedService was created form2

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -7,19 +7,33 @@ export class SeedService {
   private readonly axiosInstance: AxiosInstance = axios;
 
   async executeSeed() {
-    const res = await this.axiosInstance.get(
+    // const res = await this.axiosInstance.get(
+    //   'https://pokeapi.co/api/v2/pokemon?limit=10',
+    // );
+    // const body: PokemonRes = res?.data;
+
+    // let newPokemons = [];
+    // for (const pokemon of body?.results) {
+    //   let no = pokemon.url.split('/');
+    //   let s = no[no.length - 2];
+
+    //   newPokemons.push({ name: pokemon.name, no: +s });
+    // }
+    // // console.log(newPokemons);
+
+    const res = await this.axiosInstance.get<PokemonRes>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
-    const body: PokemonRes = res?.data;
 
-    let newPokemons = [];
-    for (const pokemon of body?.results) {
-      let no = pokemon.url.split('/');
-      let s = no[no.length - 2];
+    let data = res.data.results;
 
-      newPokemons.push({ name: pokemon.name, no: +s });
-    }
-    // console.log(newPokemons);
+    data.forEach((pokemon) => {
+      // console.log(pokemon);
+      const separateArray = pokemon.url.split('/');
+      const no = +separateArray[separateArray.length - 2];
+
+      console.log({ name: pokemon.name, no });
+    });
 
     return `seed executed!!`;
   }
